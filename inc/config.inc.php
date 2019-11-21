2db2cb98544d460bf42c6c7b96f8604059282934
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-11-21 19:10:02 +0000

    Update config.inc.php

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -3,7 +3,7 @@
 // APP
 define('APP_TITLE', 'Ceylon Railway Guards Union');
 define('APP_PRT', 'http');
-define('APP_DOM', 'www.crgu.int');
+define('APP_DOM', 'crgu.localhost');
 define('APP_FQDN', APP_PRT.'://'.APP_DOM);
 
 // DATABASE
